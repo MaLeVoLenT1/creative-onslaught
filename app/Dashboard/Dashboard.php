71bcf6ca7e6434aa76e0933b32f3608cd9a91abb
MaLeVoLenT1 <malevolent@ons-onslaught.com>
2019-10-28 11:19:09 -0400

    Initial commit

---
+++ app/Dashboard/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dione
+ * Date: 10/28/2019
+ * Time: 10:48 AM
+ */
+
+namespace App\Dashboard;
+
+
+class Dashboard
+{
+
+}
---
+++ app/Dashboard/config/Header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dione
+ * Date: 10/28/2019
+ * Time: 10:58 AM
+ */
+
+namespace App\Dashboard\config;
+
+
+class Header
+{
+
+}
