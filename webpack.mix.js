2d4cc3a5b65de46832eec4b1c1d61388bb80f47f
MaLeVoLenT1 <malevolent@ons-onslaught.com>
2019-10-30 14:30:15 -0400

    Updated webpack.mix.js with dependencies & added more javascript packages. Added the Library Javascript folder as well.

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,19 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.browserSync('localhost/CO/public');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.scripts([
+    'resources/assets/js/lib/visible.js',
+    'resources/assets/js/lib/menuzord.js',
+    'resources/assets/js/lib/jquery.nav.js',
+    'resources/assets/js/lib/wow.min.js',
+    'resources/assets/js/lib/owl.carousel.min.js',
+    'resources/assets/js/lib/smooth.js',
+    'resources/assets/js/lib/bootstrap-filestyle.min.js',
+    'resources/assets/js/lib/jquery.ui.touch-punch.mi.js',
+    'resources/assets/js/lib/jquery.fixedheadertable.js',
+], 'public/js/vendor.js');
