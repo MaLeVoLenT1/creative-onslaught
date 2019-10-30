fe1b7e0d3adaead587ea0a0ee80f96fce89e1bbc
MaLeVoLenT1 <malevolent@ons-onslaught.com>
2019-10-30 16:25:03 -0400

    Added HeaderBar and HeaderOptions vue.js files to the client.

---
+++ resources/js/components/interfaces/public/UI/HeaderBar.vue
@@ -0,0 +1,39 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "HeaderBar",
+        props:{
+            headerStyle:{'default': 'fixed'},
+            headerColor:{'default': 'dark'},
+        },
+        components:{},
+        data(){
+            return{
+                location: window.vDashboard.location,
+                // Movement Controls
+                isMoved: false, isOverlay: false, isFloating: false,
+                isCenter: false, isFluid: false, isContainer: true,
+                isDark: true, isLight: false, showIcon: false,
+            }
+        },
+        computed:{
+            showDisplay:function(){
+                if(this.isFloating && !this.isMoved) return true;
+                else return false;
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/interfaces/public/UI/HeaderOptions.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "HeaderOptions"
+    }
+</script>
+
+<style scoped>
+
+</style>
